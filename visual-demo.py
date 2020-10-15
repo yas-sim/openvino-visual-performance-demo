b4b46a17b5aabe41bcfdf8f5eb37b74ef845fcc3
Yasunori Shimura <yasu0710@gmail.com>
2020-10-16 08:22:12 +0900

    1st version

--- visual-demo.py
+++ visual-demo.py
@@ -3,6 +3,11 @@ import sys
 import glob
 import time
 import argparse
+import threading
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 import cv2
 import numpy as np
@@ -12,26 +17,35 @@ from openvino.inference_engine import IECore
 
 inf_count = 0
 
+canvas = np.zeros([0], dtype=np.uint8)
+
 class FullScreenCanvas:
     def __init__(self, winname='noname', shape=(1080, 1920, 3), full_screen=True):
+        global canvas
         self.shape   = shape
         self.winname = winname
-        self.canvas  = np.zeros((self.shape), dtype=np.uint8)
-        if full_screen:
-            cv2.namedWindow(self.winname, cv2.WINDOW_NORMAL)
-            cv2.setWindowProperty(self.winname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        canvas  = np.zeros((self.shape), dtype=np.uint8)
+        #if full_screen:
+        #    cv2.namedWindow(self.winname, cv2.WINDOW_NORMAL)
+        #    cv2.setWindowProperty(self.winname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     
     def __del__(self):
-        cv2.destroyAllWindows()
+        #cv2.destroyAllWindows()
+        return
 
     def update(self):
-        cv2.imshow(self.winname, self.canvas)
+        #cv2.imshow(self.winname, self.canvas)
+        return
 
     def getROI(self, x0, y0, x1, y1):
-        return self.canvas[y0:y1, x0:x1, :]
+        global canvas
+        #return self.canvas[y0:y1, x0:x1, :]
+        return canvas[y0:y1, x0:x1, :]
 
     def setROI(self, img, x0, y0, x1, y1):
-        self.canvas[y0:y1, x0:x1, :] = img
+        global canvas
+        #self.canvas[y0:y1, x0:x1, :] = img
+        canvas[y0:y1, x0:x1, :] = img
     
     def displayOcvImage(self, img, x, y):
         h,w,_ = img.shape
@@ -86,6 +100,7 @@ class BenchmarkCanvas(FullScreenCanvas):
         self.setROI(self.marker_img, x0, y0, x1, y1)
 
     def displayLogo(self):
+        global canvas
         stsY = self.grid_height * self.grid_row
         gs = self.sts_grid_size
 
@@ -105,9 +120,11 @@ class BenchmarkCanvas(FullScreenCanvas):
             tmpimg = cv2.resize(tmpimg, None, fx=(gs*4)/h, fy=(gs*4)/h) 
             self.displayOcvImage(tmpimg, gs*32, stsY+gs*7)
         else:
-            cv2.putText(self.canvas, 'OpenVINO', (gs*32, stsY+gs*9), cv2.FONT_HERSHEY_PLAIN, 5, (255,255,255), 5)
+            #cv2.putText(self.canvas, 'OpenVINO', (gs*32, stsY+gs*9), cv2.FONT_HERSHEY_PLAIN, 5, (255,255,255), 5)
+            cv2.putText(canvas, 'OpenVINO', (gs*32, stsY+gs*9), cv2.FONT_HERSHEY_PLAIN, 5, (255,255,255), 5)
 
     def displayModel(self, modelName, device, batch, skip_count):
+        global canvas
         _, name = os.path.split(modelName)
         name,_ = os.path.splitext(name)
         stsY = self.grid_height * self.grid_row
@@ -116,18 +133,22 @@ class BenchmarkCanvas(FullScreenCanvas):
         tt = self.disp_res[0] / 960         # text thickness
         tt = int(max(tt,1))
         txt = 'model: {} ({})'.format(name, device)
-        cv2.putText(self.canvas, txt, (gs*1, stsY+gs* 8), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
+        #cv2.putText(self.canvas, txt, (gs*1, stsY+gs* 8), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
+        cv2.putText(canvas, txt, (gs*1, stsY+gs* 8), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
         txt = 'batch: {}, skip frame: {}'.format(batch, skip_count)
-        cv2.putText(self.canvas, txt, (gs*1, stsY+gs*10), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
+        #cv2.putText(self.canvas, txt, (gs*1, stsY+gs*10), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
+        cv2.putText(canvas, txt, (gs*1, stsY+gs*10), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
 
     # elapse = sec
     def dispProgressBar(self, curItr, ttlItr, elapse, max_fps=100):
+        global canvas
         def progressBar(img, x0, y0, x1, y1, val, color):
             val = min(100, val)
             xx = int(((x1-x0)*val)/100)+x0
             cv2.rectangle(img, (x0,y0), (xx,y1), color, -1)
             cv2.rectangle(img, (xx,y0), (x1,y1), (32,32,32), -1)
-        img = self.canvas
+        #img = self.canvas
+        img = canvas
 
         stsY  = self.grid_height * self.grid_row
         gs = self.sts_grid_size             # status pane grid size (dot)
@@ -148,7 +169,6 @@ class BenchmarkCanvas(FullScreenCanvas):
         cv2.putText(img, 'Time: {:5.1f}'.format(elapse), (gs*66, stsY+gs*8), cv2.FONT_HERSHEY_PLAIN, ts, (255,255,255), tt)
 
 
-import threading
 class benchmark():
     def __init__(self, model, device='CPU', nireq=4, config=None):
         self.config = config
@@ -219,34 +239,10 @@ class benchmark():
         for key in cfg_keys:
             print('   ', key, self.exenet.get_config(key))
 
-        # Create benchmark_loop thread and run it
-        th = threading.Thread(target=self.benchmark_loop, kwargs={'niter':niter, 'nireq':nireq, 'files':files, 'max_fps':max_fps})
         self.thread_abort_flag = False
-        start = time.perf_counter()
-        th.start()
-        while th.is_alive():
-            cv2.imshow(self.canvas.winname, self.canvas.canvas)
-            key = cv2.waitKey(100)
-            if key == 27:
-                self.thread_abort_flag = True
-                break
-        th.join()
-        end = time.perf_counter()
-
-        if self.thread_abort_flag == False:
-            # Display the rsult
-            print('Time: {:8.2f} sec, Throughput: {:8.2f} inf/sec'.format(end-start, niter/(end-start)))
-            self.canvas.dispProgressBar(curItr=niter, ttlItr=niter, elapse=end-start, max_fps=max_fps)
-            cv2.imshow(self.canvas.winname, self.canvas.canvas)
-            cv2.waitKey(5 * 1000)    # wait for 5 sec
-        else:
-            print('Benchmark aborted')
-
-
-    def benchmark_loop(self, niter, nireq, files, max_fps):
-        # Actual benchmarking thread
         niter = (niter//self.batch)*self.batch + (self.batch if niter % self.batch else 0)  # tweak number of iteration for batch inferencing
         self.inf_count = 0
+
         start = time.perf_counter()
         # Do inference
         for i in range(0, niter, self.batch):
@@ -277,6 +273,18 @@ class benchmark():
                 return
         # Wait for completion of all infer requests
         while self.inf_count < niter:   pass
+
+        end = time.perf_counter()
+
+        if self.thread_abort_flag == False:
+            # Display the rsult
+            print('Time: {:8.2f} sec, Throughput: {:8.2f} inf/sec'.format(end-start, niter/(end-start)))
+            self.canvas.dispProgressBar(curItr=niter, ttlItr=niter, elapse=end-start, max_fps=max_fps)
+            cv2.imshow(self.canvas.winname, self.canvas.canvas)
+            cv2.waitKey(5 * 1000)    # wait for 5 sec
+        else:
+            print('Benchmark aborted')
+
 
 
 
@@ -334,6 +342,59 @@ class benchmark_ssd(benchmark):
 
 
 
+def draw():
+    global canvas
+    img= cv2.cvtColor(canvas,cv2.COLOR_BGR2RGB) #BGR-->RGB
+    h, w = img.shape[:2]
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, img)
+
+    #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)
+
+    # Enable texture map
+    glEnable(GL_TEXTURE_2D)
+    # Set texture map method
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    # draw square
+    glBegin(GL_QUADS) 
+    glTexCoord2d(0.0, 1.0)
+    glVertex3d(-1.0, -1.0,  0.0)
+    glTexCoord2d(1.0, 1.0)
+    glVertex3d( 1.0, -1.0,  0.0)
+    glTexCoord2d(1.0, 0.0)
+    glVertex3d( 1.0,  1.0,  0.0)
+    glTexCoord2d(0.0, 0.0)
+    glVertex3d(-1.0,  1.0,  0.0)
+    glEnd()
+
+    glFlush();
+    glutSwapBuffers()
+
+def init():
+    glClearColor(0.7, 0.7, 0.7, 0.7)
+
+def idle():
+    glutPostRedisplay()
+
+def reshape(w, h):
+    glViewport(0, 0, w, h)
+    glLoadIdentity()
+    #Make the display area proportional to the size of the view
+    glOrtho(-w / 1920, w / 1920, -h / 1080, h / 1080, -1.0, 1.0)
+
+def keyboard(key, x, y):
+    # convert byte to str
+    key = key.decode('utf-8')
+    # press q to exit
+    if key == 'q':
+        print('exit')
+        sys.exit()
+
+
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', default='default.yml', type=str, help='Input configuration file (YAML)')
@@ -363,8 +424,27 @@ def main():
     else:
         print('ERROR: Unsupported type of model specified. ({})'.format(model_type))
         return 1
+
     bm.preprocessImages(files)
-    bm.run(niter=config['iteration'], nireq=config['num_requests'], max_fps=config['fps_max_value'])
+    th = threading.Thread(target=bm.run, kwargs={
+        'niter'  :config['iteration'], 
+        'nireq'  :config['num_requests'], 
+        'max_fps':config['fps_max_value']})
+    th.start()
+
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(1920, 1080);
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE )
+    #glutCreateWindow("Display")
+    glutEnterGameMode()
+    glutDisplayFunc(draw)
+    glutReshapeFunc(reshape)
+    glutKeyboardFunc(keyboard)
+    init()
+    glutIdleFunc(idle)
+    glutMainLoop()
+
     return 1
 
 if __name__ == '__main__':
